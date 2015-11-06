9561d20adfe9751f42b300a6a957a8ae8f99c76d
Mike Davis <mike@michaeljacobdavis.com>
2015-11-06 15:50:34 -0800

    Added test base

---
+++ test/utils/query.js
@@ -0,0 +1,14 @@
+var Lab = require('lab');
+var Code = require('code');
+var query = require('../../lib/utils/query');
+var lab = exports.lab = Lab.script();
+
+// Alias
+var afterEach = lab.afterEach;
+var beforeEach = lab.beforeEach;
+var describe = lab.describe;
+var expect = Code.expect;
+var it = lab.it;
+
+describe('Query', function () {
+});
